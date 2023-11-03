0688f63d989163fe4b111acf94fd1c7b278e31d0
vanessavpg <vanessa.pg026@gmail.com>
2023-11-03 20:18:32 -0300

    feat: carousel

--- app/components/Product.tsx
+++ app/components/Product.tsx
@@ -3,11 +3,12 @@ import Image from 'next/image'
 interface ProductProps {
   name: string,
   src: string,
-  price: number
+  price: number,
+  className: string
 }
-export default function Product({ src, name, price }: ProductProps) {
+export default function Product({ src, name, price, className }: ProductProps) {
   return (
-    <section className='group relative flex items-center justify-center rounded-lg bg-gradient shadow-lg overflow-hidden p-1 cursor-pointer min-w-max'>
+    <section className={`group relative flex items-center justify-center rounded-lg bg-gradient shadow-lg overflow-hidden p-1 cursor-pointer min-w-max ${className}`}>
       <Image src={src} width={480} height={520} alt='camiseta' className='mx-20' />
       <footer className='absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-opacity-60 bg-black p-8 translate-y-[110%] opacity-0 transition-all ease-in-out duration-200 group-hover:translate-y-0 group-hover:opacity-100'>
         <strong className='text-lg'>{name}</strong>
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
+'use client';
 import Product from "./components/Product";
 import { products } from "./lib/products";
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides:
+    {
+      perView: 2,
+      spacing: 48,
+    }
+  })
+
   return (
-    <main className="flex gap-12 calc-max-width ml-auto min-h-[41rem]">
+    <main ref={sliderRef} className="keen-slider flex calc-max-width ml-auto min-h-[41rem]">
       {
         products.map((product) => {
           return (
-            <Product src={product.src} key={product.id} name={product.name} price={product.price}/>
+            <Product className="keen-slider__slide" src={product.src} key={product.id} name={product.name} price={product.price} />
           )
+
+
         }
 
         )
