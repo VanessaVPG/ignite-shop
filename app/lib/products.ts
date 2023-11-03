e6ddb38fa7485f3e69510c36e399d4f62ecb81b3
vanessavpg <vanessa.pg026@gmail.com>
2023-11-03 20:07:15 -0300

    feat: produtos

---
+++ app/lib/products.ts
@@ -0,0 +1,20 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Camiseta',
+    price: 49.99,
+    src: '/assets/camisetas/camiseta-1.png'
+  },
+  {
+    id: 2,
+    name: 'Camiseta',
+    price: 99.99,
+    src: '/assets/camisetas/camiseta-2.png'
+  },
+  {
+    id: 3,
+    name: 'Camiseta',
+    price: 89.99,
+    src: '/assets/camisetas/camiseta-3.png'
+  },
+]
